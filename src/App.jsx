f5d01e8903b249517a90d10a55430a5c93e67281
성혜람 <wodnr624@gmail.com>
2025-07-10 16:53:42 +0900

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import Home from "./Pages/Home";
-import Diary from "./Pages/Diary";
-import New from "./Pages/New";
-import Notfound from "./Pages/Notfound";
-import Edit from "./Pages/Edit";
 import { Route, Routes } from "react-router-dom";
 import { useReducer, useRef, createContext } from "react";
 import "./App.css";
