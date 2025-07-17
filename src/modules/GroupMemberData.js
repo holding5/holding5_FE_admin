600fd2aab367277ae5eacb9654fcffcc65c8ea78
추동훈 <cdh5113@naver.com>
2025-07-17 21:48:22 +0900

    feature/group

---
+++ src/modules/GroupMemberData.js
@@ -0,0 +1,9 @@
+import dogImage from '../assets/dog.jpg';
+
+const GroupMemberData = [
+    {groupId: "u001", id:1, img:dogImage, name:"추동훈", nickName:"추"},
+    {groupId: "u001", id:2, img:dogImage, name:"성혜람", nickName:"성"},
+    {groupId: "u002", id:3, img:dogImage, name:"홍길동", nickName:"홍길동"},
+]
+
+export default GroupMemberData;
